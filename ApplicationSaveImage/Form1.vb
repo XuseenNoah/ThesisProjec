b62be0599d49e83ca9e9bd63ed271c6648d04568
xuseennoah <lect2017@gmail.com>
2017-10-02 07:51:59 -0700

    well done

---
+++ ApplicationSaveImage/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Data.SqlClient
+Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class Form1
+    Dim conn As New SqlConnection(" server = .\; database = amaano ; user = sa ; password = 1234 ")
+
+    Dim cmd As New SqlCommand
+    Dim da As New SqlDataAdapter("select * from users", conn)
+    Dim ds As New DataSet
+    Dim maxrow As Integer
+    Dim i As Integer
+    Dim dr As SqlDataReader
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim fopen As New OpenFileDialog
+            fopen.FileName = ""
+            fopen.Filter = "Image Files (*.jpg)|*.jpg|(*.jpeg)|*.JPEG|(*.gif)|*.gif|(*.png)|*.png|All Files (*.*)|*.*"
+            fopen.ShowDialog()
+            PictureBox1.Image = System.Drawing.Bitmap.FromFile(fopen.FileName)
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = System.Drawing.Bitmap.FromFile(Application.StartupPath & "\NoImage\" & "noimage.gif")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        conn.Close()
+        conn.Open()
+        'product picture
+        Dim ms As New MemoryStream
+        PictureBox1.Image.Save(ms, ImageFormat.Jpeg)
+        'Read from Memorystream into byte Array.
+        Dim bytBLOBData(ms.Length - 1) As Byte
+        ms.Position = 0
+        ms.Read(bytBLOBData, 0, Convert.ToInt32(ms.Length))
+
+        Using cmd As New SqlClient.SqlCommand("insert into users(id,name,shop,role,username,password,mobile,pic,active,date,createduser) values ('" & idtxt.Text & "' , '" & nametxt.Text & "' , '" & titlecom.Text & "' , '" & Rolecomp.Text & "' , '" & usernametxt.Text & "' , '" & passwordtxt.Text & "' ,'" & mobiletxt.Text & "', @pic , 'A','" & datetxt.Text & "','" & MDIParent1.userlbl.Text & "')", conn)
+
+            Dim prmstudent1 As New SqlParameter("@pic", SqlDbType.VarBinary, bytBLOBData.Length, ParameterDirection.Input, False, 0, 0, Nothing, DataRowVersion.Current, bytBLOBData)
+
+            cmd.Parameters.Add(prmstudent1)
+
+            converimage()
+            'cmd.Parameters.Add(New SqlClient.SqlParameter("@productpict", SqlDbType.Image)).Value = IO.File.ReadAllBytes(a.FileName)
+
+            i = cmd.ExecuteNonQuery
+
+        End Using
+    End Sub
+
+    Private Sub converimage()
+        If PictureBox1.Image Is Nothing Then
+            Dim MS = New MemoryStream
+            PictureBox1.Image.Save(MS, ImageFormat.Jpeg)
+            Dim photo_aray(MS.Length - 1) As Byte
+            MS.Position = 0
+            MS.Read(photo_aray, 0, photo_aray.Length)
+            cmd.Parameters.AddWithValue("@som", photo_aray)
+        End If
+    End Sub
+End Class
---
+++ ApplicationSaveImage/Login.vb
@@ -0,0 +1,141 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Login
+    Dim con As SqlConnection = New SqlConnection(cnString)
+    'Dim con As New SqlConnection("server = ./; database = Amaano ; user = sa ; password = 1234 ")
+    Dim cmd As New SqlCommand
+    Dim da As New SqlDataAdapter
+    Dim ds As DataSet
+    Dim dr As SqlDataReader
+    Dim s As Integer = 0
+    Dim r As String
+    Dim per As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MsgBox(" fadlan gali  user name ka ")
+            TextBox1.Focus()
+
+        ElseIf TextBox2.Text = "" Then
+            MsgBox(" fadlan gali password ka ")
+            TextBox2.Focus()
+
+        Else
+
+
+            con.Close()
+
+
+            Dim str As String = " select name, username  , password ,role,username  from users where username = '" & UsernameTextBox.Text & "' and password = '" & PasswordTextBox.Text & "'"
+            cmd = New SqlCommand(str, con)
+            con.Open()
+            dr = cmd.ExecuteReader()
+            If dr.Read = True Then
+                'MsgBox(" succefully login ")
+                Me.Hide()
+                MDIParent1.Show()
+
+                MDIParent1.userlbl.Text = dr(0)
+                MDIParent1.Rolelbl.Text = dr(3)
+                MDIParent1.Label1.Text = dr(4)
+                per = dr("role").ToString()
+
+                If per = "Cashier" Then
+                    'MDIParent1.Invntrytoolstrip.Enabled = False
+
+                    'MDIParent1.ReportsToolStripMenuItem2.Enabled = False
+                    MDIParent1.craateuser.Enabled = False
+                    'MDIParent1.DeleteUserToolStripMenuItem.Enabled = False
+
+                    'MDIParent1.backkupToolStripMenuItem.Enabled = False
+                    'MDIParent1.btn1.Enabled = False
+                    'MDIParent1.btn2.Enabled = False
+                    'MDIParent1.btn3.Enabled = False
+                    'MDIParent1.btn4.Enabled = False
+                    'MDIParent1.btn5.Enabled = False
+                    'MDIParent1.btn6.Enabled = False
+                    'MDIParent1.btn7.Enabled = False
+                ElseIf per = "Manager" Then
+                    ''MDIParent1.EmployeeRegistratioToolStripMenuItem.Enabled = False
+
+                    ''MDIParent1.ReportsToolStripMenuItem2.Enabled = False
+                    'MDIParent1.craateuser.Enabled = False
+                    'MDIParent1.DeleteUserToolStripMenuItem.Enabled = False
+
+                    ''MDIParent1.backkupToolStripMenuItem.Enabled = False
+                    'MDIParent1.craateuser.Enabled = False
+                    'MDIParent1.DeleteUserToolStripMenuItem.Enabled = False
+                    ''MDIParent1.btn1.Enabled = False
+                    ''MDIParent1.btn2.Enabled = False
+                    ''MDIParent1.btn3.Enabled = False
+                    ''MDIParent1.btn4.Enabled = False
+                    ''MDIParent1.btn5.Enabled = False
+                    'MDIParent1.btn6.Enabled = False
+                    'MDIParent1.btn7.Enabled = False
+                ElseIf per = "Accountant" Then
+
+                    'MDIParent1.Invntrytoolstrip.Enabled = False
+
+                    'MDIParent1.ReportsToolStripMenuItem2.Enabled = False
+                    MDIParent1.craateuser.Enabled = False
+                    'MDIParent1.DeleteUserToolStripMenuItem.Enabled = False
+
+                    MDIParent1.backkupToolStripMenuItem.Enabled = False
+                    'MDIParent1.btn1.Enabled = False
+                    'MDIParent1.btn2.Enabled = False
+                    MDIParent1.btn3.Enabled = False
+                    MDIParent1.btn4.Enabled = False
+                    'MDIParent1.btn5.Enabled = False
+                    'MDIParent1.btn6.Enabled = False
+                    'MDIParent1.btn7.Enabled = False
+
+                Else
+
+
+                    MDIParent1.ReportsToolStripMenuItem2.Enabled = False
+                    'MDIParent1.Invntrytoolstrip.Enabled = False
+                    'MDIParent1.Salestoolstrip.Enabled = False
+
+                    MDIParent1.backkupToolStripMenuItem.Enabled = False
+                    MDIParent1.SecurityToolStripMenuItem.Enabled = False
+                    'MDIParent1.DeleteUserToolStripMenuItem.Enabled = False
+                    MDIParent1.btn1.Enabled = False
+                    MDIParent1.btn2.Enabled = False
+                    MDIParent1.btn3.Enabled = False
+                    MDIParent1.btn4.Enabled = False
+                    MDIParent1.btn5.Enabled = False
+                    MDIParent1.btn6.Enabled = False
+                    MDIParent1.btn7.Enabled = False
+                End If
+            Else
+                If s >= 3 Then
+                    MsgBox(" fadlan isku day  mar danbe ", MsgBoxStyle.Information, " macasalam ")
+
+                    End
+
+
+                End If
+                MsgBox(" user ka ama passwordku waa khalad ")
+                TextBox1.Clear()
+                TextBox2.Clear()
+
+                TextBox1.Focus()
+                s += 1
+
+
+            End If
+
+
+
+
+        End If
+
+
+
+        'dr.Close()
+        If con.State = ConnectionState.Connecting Then con.Close()
+
+
+
+        'Me.Close()
+    End Sub
+End Class
